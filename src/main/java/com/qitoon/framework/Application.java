73e0c8ca4ba067f4bbbbd4d58dba5151a80d07a9
黄杰 <huangjie@syswin.com>
2016-10-25 16:26:59 +0800

    git

--- src/main/java/com/qitoon/framework/Application.java
+++ src/main/java/com/qitoon/framework/Application.java
@@ -49,7 +49,8 @@ public class Application {
 
 
     /**
-     * Start
+     *  start
+     * @param args
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
